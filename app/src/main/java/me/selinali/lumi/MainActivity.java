9f85229e81bad26c204696c305240866b8bf4050
Selina <li.selina123@gmail.com>
2015-11-25 21:24:13 -0500

    Add working switch

--- app/src/main/java/me/selinali/lumi/MainActivity.java
+++ app/src/main/java/me/selinali/lumi/MainActivity.java
@@ -1,13 +1,40 @@
 package me.selinali.lumi;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    Camera camera;
+    Boolean isLightOn;
+    Switch lightSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isLightOn = false;
+        lightSwitch = (Switch) findViewById(R.id.light_switch);
+        lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    camera = Camera.open();
+                    Camera.Parameters parameters = camera.getParameters();
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(parameters);
+                    camera.startPreview();
+                    isLightOn = true;
+                } else {
+                    camera.stopPreview();
+                    camera.release();
+                    isLightOn = false;
+                }
+            }
+        });
     }
 }
