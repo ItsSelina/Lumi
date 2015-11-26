eb0aa541ab99827325fe915d16ffa91eb9d2f9df
Selina <li.selina123@gmail.com>
2015-11-25 21:52:41 -0500

    Change color

--- app/src/main/java/me/selinali/lumi/MainActivity.java
+++ app/src/main/java/me/selinali/lumi/MainActivity.java
@@ -1,8 +1,10 @@
 package me.selinali.lumi;
 
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     Camera camera;
     Boolean isLightOn;
     Switch lightSwitch;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
         isLightOn = false;
         lightSwitch = (Switch) findViewById(R.id.light_switch);
+        view = findViewById(R.id.background);
+
         lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
+                    view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                     camera = Camera.open();
                     Camera.Parameters parameters = camera.getParameters();
                     parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     camera.stopPreview();
                     camera.release();
+                    view.setBackgroundColor(getResources().getColor(R.color.gray));
                     isLightOn = false;
                 }
             }
