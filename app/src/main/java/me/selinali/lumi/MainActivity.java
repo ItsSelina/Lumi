e8434e3a61a4f3e73275ca7f9f3e8d9f141c2b6d
Selina <li.selina123@gmail.com>
2015-12-07 13:44:50 -0500

    Fix crash on marshmallow and add blurry drawables

--- app/src/main/java/me/selinali/lumi/MainActivity.java
+++ app/src/main/java/me/selinali/lumi/MainActivity.java
@@ -1,7 +1,11 @@
 package me.selinali.lumi;
 
-import android.graphics.Color;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +14,15 @@ import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    CameraManager cameraManager;
+    String[] camId;
     Camera camera;
     Boolean isLightOn;
     Switch lightSwitch;
     View view;
+    boolean isMarshmallow;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +31,53 @@ public class MainActivity extends AppCompatActivity {
         isLightOn = false;
         lightSwitch = (Switch) findViewById(R.id.light_switch);
         view = findViewById(R.id.background);
+        isMarshmallow = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+
+        if (isMarshmallow) {
+            cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try {
+                camId = cameraManager.getCameraIdList();
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
 
         lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @TargetApi(Build.VERSION_CODES.M)
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                    camera = Camera.open();
-                    Camera.Parameters parameters = camera.getParameters();
-                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                    camera.setParameters(parameters);
-                    camera.startPreview();
-                    isLightOn = true;
+                    view.setBackgroundResource(R.drawable.lights_on_copy);
+
+                    if (isMarshmallow) {
+                        try {
+                            cameraManager.setTorchMode(camId[0], true);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        camera = Camera.open();
+                        Camera.Parameters parameters = camera.getParameters();
+                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(parameters);
+                        camera.startPreview();
+                        isLightOn = true;
+                    }
+
                 } else {
-                    camera.stopPreview();
-                    camera.release();
-                    view.setBackgroundColor(getResources().getColor(R.color.gray));
-                    isLightOn = false;
+                    view.setBackgroundResource(R.drawable.lights_off_copy);
+
+                    if (isMarshmallow) {
+                        try {
+                            cameraManager.setTorchMode(camId[0], false);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        camera.stopPreview();
+                        camera.release();
+                        isLightOn = false;
+                    }
                 }
             }
         });
